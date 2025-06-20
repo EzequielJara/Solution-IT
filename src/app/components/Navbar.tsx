4f0bb83dc9b6851609cb8d44f34c576e2ea197fa
EzequielJara <ezequierjaraepet20@gmail.com>
2025-06-19 22:58:11 -0300

    update navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,52 +13,52 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="w-full bg-[#05A4C0] text-white">
-     <div className="max-w-7xl mx-auto flex justify-between items-center py-4 px-4">
-      <h1 className="text-2xl font-light text-white animate-pulse">
-        Soluciones Tecnológicas
-      </h1>
+    <header className="w-full bg-[#05A4C0] text-white z-50 relative ">
+      <div className="max-w-7xl mx-auto flex justify-between items-center py-4 px-4 relative">
+        <h1 className="text-2xl font-light animate-pulse">
+          Soluciones Tecnológicas
+        </h1>
 
-      {/* Botón hamburguesa */}
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="md:hidden flex flex-col justify-between w-6 h-6 focus:outline-none mr-4"
-        aria-label="Toggle menu"
-      >
-        <span className={`block h-0.5 w-full bg-white transform transition duration-300 ease-in-out ${isOpen ? "rotate-45 translate-y-1.5" : ""}`} />
-        <span className={`block h-0.5 w-full bg-white transition duration-300 ease-in-out ${isOpen ? "opacity-0" : "opacity-100"}`} />
-        <span className={`block h-0.5 w-full bg-white transform transition duration-300 ease-in-out ${isOpen ? "-rotate-45 -translate-y-4" : ""}`} />
-      </button>
+        {/* Botón hamburguesa */}
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="md:hidden flex flex-col justify-between w-6 h-6 focus:outline-none"
+          aria-label="Toggle menu"
+        >
+          <span className={`block h-0.5 w-full bg-white transform transition duration-300 ease-in-out ${isOpen ? "rotate-45 translate-y-1.5" : ""}`} />
+          <span className={`block h-0.5 w-full bg-white transition duration-300 ease-in-out ${isOpen ? "opacity-0" : "opacity-100"}`} />
+          <span className={`block h-0.5 w-full bg-white transform transition duration-300 ease-in-out ${isOpen ? "-rotate-45 -translate-y-4" : ""}`} />
+        </button>
 
-      {/* Navegación desktop */}
-      <nav className="hidden md:flex space-x-8 text-base text-white font-semibold z-50">
-        {menuItems.map((item, index) => (
-          <a
-            key={index}
-            href={item.href}
-            className="relative inline-block transition duration-300 ease-in-out hover:text-[#4572a5]"
-          >
-            {item.label}
-            <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-[#1c608d] scale-x-0 hover:scale-x-100 origin-center transition-transform duration-300"></span>
-          </a>
-        ))}
-      </nav>
-
-      {/* Menú móvil */}
-      {isOpen && (
-        <nav className="absolute top-full left-0 w-full bg-[#05A4C0] flex flex-col space-y-4 py-4 px-6 md:hidden z-50">
+        {/* Navegación desktop */}
+        <nav className="hidden md:flex space-x-8 text-base font-semibold">
           {menuItems.map((item, index) => (
             <a
               key={index}
               href={item.href}
-              className="text-white font-semibold text-lg"
-              onClick={() => setIsOpen(false)}
+              className="relative inline-block transition duration-300 ease-in-out hover:text-[#4572a5]"
             >
               {item.label}
+              <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-[#1c608d] scale-x-0 hover:scale-x-100 origin-center transition-transform duration-300"></span>
             </a>
           ))}
         </nav>
-      )}
+
+        {/* Menú móvil */}
+        {isOpen && (
+          <nav className="absolute top-full left-0 w-full bg-[#05A4C0] flex flex-col space-y-4 py-4 px-6 md:hidden ">
+            {menuItems.map((item, index) => (
+              <a
+                key={index}
+                href={item.href}
+                className="text-white font-semibold text-lg"
+                onClick={() => setIsOpen(false)}
+              >
+                {item.label}
+              </a>
+            ))}
+          </nav>
+        )}
       </div>
     </header>
   );
